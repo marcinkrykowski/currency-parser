99ae3529d1c095ac65b4da7d248b914cf52a7211
Marcin Krykowski <12751601+marcinkrykowski@users.noreply.github.com>
2020-09-06 11:27:37 +0100

    Add tests for Currency Service

--- src/main/scala/service/CurrencyService.scala
+++ src/main/scala/service/CurrencyService.scala
@@ -4,7 +4,72 @@ import repository.CurrencyRepository
 
 case class CurrencyService(currencyRepository: CurrencyRepository) {
 
-  def ratesWithinDates(
+  def averageRate(
+      start: String,
+      finish: String,
+      currency: String
+  ): Any = {
+    val rates: List[BigDecimal] =
+      currencyRatesWithinDates(start, finish, currency)
+        .flatMap(rate => toDouble(rate))
+        .map(rateValue => BigDecimal(rateValue))
+
+    // below line prevents wrong calculation
+    // when for example if some currency misses data from one day
+    // we do not want to calculate missing values as 0
+    val length = rates.count(x => x != BigDecimal(0.00))
+
+    rates match {
+      case Nil => "Data not available"
+      case _ =>
+        (rates.sum / length).setScale(4, BigDecimal.RoundingMode.HALF_UP)
+    }
+  }
+
+  def highestRate(
+      start: String,
+      finish: String,
+      currency: String
+  ): String = {
+    currencyRatesWithinDates(start, finish, currency).max
+  }
+
+  def makeExchange(
+      date: String,
+      sourceCurrency: String,
+      targetCurrency: String,
+      amount: BigDecimal
+  ): Any = {
+    val ratesForGivenDate = ratesForDate(date)
+    val rateForSourceCurrency = BigDecimal(
+      ratesForGivenDate.getOrElse(sourceCurrency, "0").toDouble
+    )
+    val rateForTargetCurrency = BigDecimal(
+      ratesForGivenDate.getOrElse(targetCurrency, "0").toDouble
+    )
+
+    try {
+      val result: BigDecimal =
+        (amount * rateForSourceCurrency) / rateForTargetCurrency
+      List(
+        result.setScale(2, BigDecimal.RoundingMode.HALF_UP),
+        BigDecimal(0.00)
+      ).max //to prevent negative amount calculation
+    } catch {
+      case _: Exception => "Data not available"
+    }
+
+  }
+
+  def toDouble(s: String): Option[Double] = {
+    try {
+      Some(s.toDouble)
+    } catch {
+      case _: NumberFormatException => None
+    }
+  }
+
+  def currencyRatesWithinDates(
       start: String,
       finish: String,
       currency: String
@@ -20,65 +85,18 @@ case class CurrencyService(currencyRepository: CurrencyRepository) {
         .reverse
         .dropWhile(rec => rec.date != finish)
 
-    val res =
-      afterStart
-        .intersect(beforeEnd)
-        .map(rec => rec.rates(currency))
-//        .map(rec => rec.rates.getOrElse(currency, "No such currency"))
-    res
-  }
-  def averageRate(
-      start: String,
-      finish: String,
-      currency: String
-  ): BigDecimal = {
-    val odp =
-      ratesWithinDates(start, finish, currency).map(x => BigDecimal(x.toDouble))
-    odp.sum / odp.length
-  }
-
-  def toDouble(s: String): Option[Double] = {
-    try {
-      Some(s.toDouble)
-    } catch {
-      case e: NumberFormatException => None
-    }
-  }
-
-  def highestRate(
-      start: String,
-      finish: String,
-      currency: String
-  ): String = {
-    val odp = ratesWithinDates(start, finish, currency).max
-    odp
-  }
-
-  def makeExchange(
-      date: String,
-      sourceCurrency: String,
-      targetCurrency: String,
-      amount: BigDecimal
-  ): BigDecimal = {
-    val ratesForGivenDate = ratesForDate(date)
-    val rateForSourceCurrency = BigDecimal(
-      ratesForGivenDate(sourceCurrency).toDouble
-    )
-    val rateForTargetCurrency = BigDecimal(
-      ratesForGivenDate(targetCurrency).toDouble
-    )
-    //TODO what if there will be no rate eg "N/A"
-    val res: BigDecimal = amount * rateForSourceCurrency / rateForTargetCurrency
-    res.setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    afterStart
+      .intersect(beforeEnd)
+      .map(rec => rec.rates.getOrElse(currency, "Currency does not exist"))
   }
 
   def ratesForDate(date: String): Map[String, String] = {
-    val res = currencyRepository.records
+    val rates = currencyRepository.records
       .filter(record => record.date.equals(date))
       .map(record => record.rates)
-    res match {
+    rates match {
       case Nil => Map.empty
-      case _   => res.head
+      case _   => rates.head
     }
   }
 }
---
+++ src/test/scala/service/CurrencyServiceSpec.scala
@@ -0,0 +1,148 @@
+package service
+
+import model.CurrencyRecord
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import repository.CurrencyRepository
+
+class CurrencyServiceSpec extends AnyFlatSpec with Matchers {
+  val firstRecord: CurrencyRecord = CurrencyRecord(
+    "2020-08-28",
+    Map(
+      "USD" -> "1.9999",
+      "JPY" -> "125.39",
+      "BGN" -> "1.9558",
+      "CYP" -> "N/A"
+    )
+  )
+  val secondRecord: CurrencyRecord = CurrencyRecord(
+    "2020-08-27",
+    Map(
+      "USD" -> "1.1806",
+      "JPY" -> "125.34",
+      "BGN" -> "N/A",
+      "CYP" -> "N/A"
+    )
+  )
+  val thirdRecord: CurrencyRecord = CurrencyRecord(
+    "2020-08-26",
+    Map(
+      "USD" -> "1.1806",
+      "JPY" -> "125.34",
+      "BGN" -> "1.9558",
+      "CYP" -> "N/A"
+    )
+  )
+  val allRecords = List(firstRecord, secondRecord, thirdRecord)
+  val service: CurrencyService = CurrencyService(CurrencyRepository(allRecords))
+
+  it should "calculate average rate for existing currency" in {
+    service.averageRate("2020-08-26", "2020-08-27", "USD") shouldBe 1.1806
+  }
+
+  it should "return \"Data not available\" for not available rate" in {
+    service.averageRate(
+      "2020-08-26",
+      "2020-08-27",
+      "CYP"
+    ) shouldBe "Data not available"
+  }
+
+  it should "return average rate for one not available rate" in {
+    service.averageRate(
+      "2020-08-26",
+      "2020-08-28",
+      "BGN"
+    ) shouldBe 1.9558
+  }
+
+  it should "return \"Data not available\" for not existing currency in average rate" in {
+    service.averageRate(
+      "2020-08-25",
+      "2020-08-27",
+      "XXX"
+    ) shouldBe "Data not available"
+  }
+
+  it should "return highest rate for existing currency" in {
+    service
+      .highestRate("2020-08-26", "2020-08-28", "USD")
+      .toDouble shouldBe 1.9999
+  }
+
+  it should "return highest rate for not available rate" in {
+    service
+      .highestRate("2020-08-26", "2020-08-28", "CYP") shouldBe "N/A"
+  }
+
+  it should "return highest rate for not existing currency" in {
+    service
+      .highestRate(
+        "2020-08-26",
+        "2020-08-28",
+        "XXX"
+      ) shouldBe "Currency does not exist"
+  }
+
+  it should "calculate exchange value correctly" in {
+    service.makeExchange("2020-08-27", "JPY", "USD", 1.00) shouldEqual 106.17
+  }
+
+  it should "return \"Data not available\" for not existing date" in {
+    service.makeExchange(
+      "2021-08-27",
+      "JPY",
+      "USD",
+      1.00
+    ) shouldEqual "Data not available"
+  }
+
+  it should "return 0 for not existing currency in exchange" in {
+    service.makeExchange(
+      "2020-08-27",
+      "XXX",
+      "USD",
+      1.00
+    ) shouldEqual 0
+  }
+
+  it should "return 0 for negative amount" in {
+    service.makeExchange(
+      "2020-08-27",
+      "JPY",
+      "USD",
+      -1234.00
+    ) shouldEqual 0
+  }
+
+  //rates within dates
+  it should "return rates for given dates" in {
+    service.currencyRatesWithinDates(
+      "2020-08-26",
+      "2020-08-27",
+      "USD"
+    ) should contain theSameElementsAs List("1.1806", "1.1806")
+  }
+
+  it should "return empty list for not existing date" in {
+    service.currencyRatesWithinDates(
+      "2021-08-26",
+      "2020-08-27",
+      "USD"
+    ) should contain theSameElementsAs List.empty
+  }
+
+  it should "return rates only for given date" in {
+    service.ratesForDate("2020-08-26") should contain theSameElementsAs Map(
+      "USD" -> "1.1806",
+      "JPY" -> "125.34",
+      "BGN" -> "1.9558",
+      "CYP" -> "N/A"
+    )
+  }
+
+  it should "return empty map if date does not exist" in {
+    service.ratesForDate("2021-12-12") shouldBe Map.empty
+  }
+
+}
